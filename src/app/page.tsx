c6ecc6107b17385fd445fad4cf899051d33526bb
Sebola3461 <mixvideostubecontato@gmail.com>
2023-06-13 21:47:08 -0300

    Nice documentation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
+import { useEffect } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
-	fetch("/api/authorize")
-		.then((r) => r.text())
-		.then((url) => window.location.replace(url));
+	useEffect(() => {
+		fetch("/api/authorize")
+			.then((r) => r.text())
+			.then((url) => window.location.replace(url));
+	}, []);
 
 	return <main className={styles.main}>Please wait... Redirecting you</main>;
 }
